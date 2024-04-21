0777d24d611c3301cc86f7536f71b1415e5d8195
Satyajit <sahoosatyajit2801@gmail.com>
2024-04-21 23:16:46 +0530

    feat: added connecting, connect metamask wallet and upload voter image in IPFS network

--- context/Voter.js
+++ context/Voter.js
@@ -20,8 +20,65 @@ export const VotingContext = React.createContext();
 
 export const VotingProvider = ({ children }) => {
     const votingTitle = 'My first smart contract app';
+    const router = useRouter();
+    const [currentAccount, setCurrentAccount] = useState("");
+    const [candidateLength, setCandidateLength] = useState("");
+    const pushCandidate = [];
+    const candidateIndex = [];
+    const [candidateArray, setCandidateArray] = useState(pushCandidate);
+
+    // end of candidate data
+
+    const [error, setError] = useState("");
+    const highestVote = [];
+
+    // voter section
+    const pushVoter = [];
+    const [voterArray, setVoterArray] = useState(pushVoter);
+    const [voterLength, setVoterLength] = useState("");
+    const [voterAddress, setVoterAddress] = useState([]);
+
+    // connecting metamask
+    const checkIfWalletIsConnected = async () => {
+        if (!window.ethereum) return setError("Please Install Metamask!")
+
+        const account = await window.ethereum.request({ method: "eth_accounts" });
+
+        if (account.length) {
+            setCurrentAccount(account[0]);
+        } else {
+            setError('Please Install Metamask & connect, reload.');
+        }
+    }
+
+    // connect wallet
+    const connectWallet = async () => {
+        if (!window.ethereum) return setError("Please Install Metamask");
+
+        const account = await window.ethereum.request({ method: "eth_requestAccounts" });
+
+        setCurrentAccount(account[0]);
+    }
+
+    // upload to IPFS voter image
+    const uploadToIPFS = async (file) => {
+        try {
+            const added = await client.add({ content: file });
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+
+            return url;
+        } catch (error) {
+            setError('Error Uploading file to IPFS')
+        }
+    }
+
     return (
-        <VotingContext.Provider value={{ votingTitle }}>
+        <VotingContext.Provider value={{
+            votingTitle,
+            checkIfWalletIsConnected,
+            connectWallet,
+            uploadToIPFS
+        }}>
             {children}
         </VotingContext.Provider>
     )
